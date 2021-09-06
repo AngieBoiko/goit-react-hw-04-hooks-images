0095db64097ed51c497bb8ae3d2a7c484d0b76af
Anzhela Boiko <boikoanzhela@gmail.com>
2021-09-06 23:25:37 +0300

    start fix imageGallery

--- src/Components/ImageGallery/ImageGallery.jsx
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -1,106 +1,99 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from '../ImageGalleryItem';
 import Button from '../Button';
 import Modal from '../Modal/Modal';
 import Loader from '../Loader';
+import usePrevious from '../../Hooks/usePrevious';
 import PixabayFetchImages from '../../services/PixabayApi';
 import { toast } from 'react-toastify';
 
-class ImageGallery extends Component {
-  state = {
-    page: 1,
-    per_page: 12,
-    images: [],
-    error: null,
-    isLoading: null,
-    openModal: false,
-  };
-  componentDidUpdate(prevProps, prevState) {
-    const { page, per_page } = this.state;
-    const { searchQuery } = this.props;
-    if (prevProps.searchQuery !== searchQuery) {
-      this.setState({ page: 1, isLoading: true });
+export default function ImageGallery(searchQuery) {
+  const [page, setPage] = useState(1);
+  const [per_page, setPer_page] = useState(12);
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [imageForModal, setImageForModal] = useState({});
+
+  const prevPropsSearchQuery = usePrevious(searchQuery);
+  const prevPage = usePrevious(page);
+
+  useEffect(() => {
+    if (prevPropsSearchQuery !== searchQuery) {
+      setPage(1);
+      setIsLoading(true);
       PixabayFetchImages(searchQuery, page, per_page)
         .then(response => {
           if (response.hits.length > 0) {
-            this.setState({ images: response.hits });
+            setImages([...response.hits]);
           } else toast.error('Enter another word for searching!');
         })
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({ isLoading: false }));
+        .catch(error => setError({ error }))
+        .finally(() => setIsLoading(false));
     }
 
-    if (prevState.page !== page) {
-      this.setState({ isLoading: true });
-      PixabayFetchImages(searchQuery, page, per_page)
-        .then(response => {
-          if (response.hits.length > 0) {
-            this.setState(prevState => {
-              return { images: [...prevState.images, ...response.hits] };
-            });
-          } else toast.error('Enter another word for searching!');
-        })
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({ isLoading: false }));
-    }
-    this.onScroll();
-  }
+    // if (prevPage !== page) {
+    //   setIsLoading(true);
+    //   PixabayFetchImages(searchQuery, page, per_page)
+    //     .then(response => {
+    //       if (response.hits.length > 0) {
+    //         setImages(state => {
+    //           state.push(response.hits);
+    //         });
+    //       } else toast.error('Enter another word for searching!');
+    //     })
+    //     .catch(error => setError({ error }))
+    //     .finally(() => setIsLoading(false));
+    // }
+    onScroll();
+  }, [page, per_page, prevPage, prevPropsSearchQuery, searchQuery]);
 
-  onClickHandler = e => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+  const onClickHandler = e => {
+    setPage(state => state + 1);
   };
 
-  onScroll() {
+  const onScroll = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
-  }
-  onGalleryItemClick = e => {
-    this.setState({
-      openModal: true,
-      imageForModal: {
-        src: e.target.dataset.modal,
-        alt: e.target.alt,
-      },
+  };
+  const onGalleryItemClick = e => {
+    setOpenModal(true);
+    setImageForModal({
+      src: e.target.dataset.modal,
+      alt: e.target.alt,
     });
   };
-  toggleModal = () => {
-    this.setState(({ openModal }) => ({
-      openModal: !openModal,
-    }));
+  const toggleModal = () => {
+    setOpenModal(state => !state);
   };
-  render() {
-    const { isLoading, images, imageForModal, openModal } = this.state;
-    const { onGalleryItemClick, onClickHandler, toggleModal } = this;
-    return (
-      <div className="gallery-container">
-        {isLoading && <Loader />}
-        <ul className="ImageGallery" onClick={onGalleryItemClick}>
-          {images.map(item => {
-            return (
-              <ImageGalleryItem
-                key={item.id}
-                src={item.webformatURL}
-                alt={item.tags}
-                modalSrc={item.largeImageURL}
-              />
-            );
-          })}
-        </ul>
-        {images.length > 0 && <Button onClick={onClickHandler} />}
-        {openModal && (
-          <Modal forImageModal={imageForModal} onCloseModal={toggleModal} />
-        )}
-      </div>
-    );
-  }
+
+  return (
+    <div className="gallery-container">
+      {isLoading && <Loader />}
+      <ul className="ImageGallery" onClick={onGalleryItemClick}>
+        {images.map(item => {
+          return (
+            <ImageGalleryItem
+              key={item.id}
+              src={item.webformatURL}
+              alt={item.tags}
+              modalSrc={item.largeImageURL}
+            />
+          );
+        })}
+      </ul>
+      {images.length > 0 && <Button onClick={onClickHandler} />}
+      {openModal && (
+        <Modal forImageModal={imageForModal} onCloseModal={toggleModal} />
+      )}
+    </div>
+  );
 }
 
 ImageGallery.propTypes = {
   searchQuery: PropTypes.string.isRequired,
 };
-export default ImageGallery;
