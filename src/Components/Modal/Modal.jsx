d27ca2fd80b6fe03177d21d8b23c7179b524a7b1
Anzhela Boiko <boikoanzhela@gmail.com>
2021-09-09 00:20:51 +0300

    convert modal to hooks

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeydown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeydown);
-  }
-  handleKeydown = e => {
+export default function Modal({ forImageModal, onCloseModal }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeydown);
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  });
+
+  const handleKeydown = e => {
     if (e.code === 'Escape') {
-      this.props.onCloseModal();
+      onCloseModal();
     }
   };
-  handleOnClickBackdrop = e => {
+  const handleOnClickBackdrop = e => {
     if (e.currentTarget === e.target) {
-      this.props.onCloseModal();
+      onCloseModal();
     }
   };
 
-  render() {
-    const { forImageModal } = this.props;
-    return createPortal(
-      <div className="Overlay" onClick={this.handleOnClickBackdrop}>
-        <div className="Modal">
-          <img src={forImageModal.src} alt={forImageModal.alt} />
-        </div>
-      </div>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <div className="Overlay" onClick={handleOnClickBackdrop}>
+      <div className="Modal">
+        <img src={forImageModal.src} alt={forImageModal.alt} />
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
 
 Modal.propTypes = {
   forImageModal: PropTypes.object.isRequired,
   onCloseModal: PropTypes.func.isRequired,
 };
-export default Modal;
